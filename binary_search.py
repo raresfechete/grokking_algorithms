4cba4e66b9ca568da01c9f098463f0c53c2771e2
raresfechete <fechete.rares@gmail.com>
2024-07-13 02:15:17 +0300

    implemented binary search

---
+++ binary_search.py
@@ -0,0 +1,13 @@
+def binary_search(array, target):
+    left, right = 0, len(array) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return None
